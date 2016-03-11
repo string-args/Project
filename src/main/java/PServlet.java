8d0e2d22b6fe7f2aaf41fbbf013ad3475222a515
Haruhi <Haruhi>
2016-03-11 15:30:58 +0800

    Updated PServlet.java

--- src/main/java/PServlet.java
+++ src/main/java/PServlet.java
@@ -10,9 +10,14 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser; 
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import com.ibm.watson.developer_cloud.language_translation.v2.LanguageTranslation;
 import com.ibm.watson.developer_cloud.language_translation.v2.model.TranslationResult;
+import com.ibm.watson.developer_cloud.text_to_speech.v1.TextToSpeech;
+import com.ibm.watson.developer_cloud.text_to_speech.v1.model.Voice;
+
 
 @WebServlet(name = "PServlet", urlPatterns = {"/PServlet"})
 public class PServlet extends HttpServlet {
@@ -20,8 +25,14 @@ public class PServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
 		Connector conn = new Connector();
 		LanguageTranslation service = new LanguageTranslation();
+		
 		service.setUsernameAndPassword(conn.get_language_username(),conn.get_language_password());
 		TranslationResult result = service.translate(request.getParameter("input"),"en","es");
+		
+		TextToSpeech service1 = new TextToSpeech();
+		service1.setUsernameAndPassword(conn.get_t2s_username(),conn.get_t2s_password());
+		
+		String translate = null;
 		
 		try{
 			JSONParser parser = new JSONParser();
@@ -31,14 +42,27 @@ public class PServlet extends HttpServlet {
 			for (Object c : o ){
 				JSONObject p = (JSONObject) c;
 				
-				String translate = (String) p.get("translation");		
-				request.setAttribute("result",translate);
+				translate = (String) p.get("translation");		
+				break;
 			}
+			
+			String outputformat = "audio/wav";
+			InputStream speech = service1.synthesize(translate,Voice.ES_ENRIQUE,outputformat);
+			OutputStream output = response.getOutputStream();
+			byte[] buf = new byte[2046];
+			int len;
+			while ((len = speech.read(buf)) > 0){
+				output.write(buf,0,len);
+			}
+			response.setContentType("audio/wav");
+			response.setHeader("Content-disposition","attachment;filename=output.wav");
+			OutputStream os = output;
+			os.flush();
+			os.close();
 
 		} catch (Exception e){
 			e.printStackTrace(System.err);
 		}
-		response.setContentType("text/html"); 
 		response.setStatus(200); 
  		request.getRequestDispatcher("index.jsp").forward(request,response); 
 	}
